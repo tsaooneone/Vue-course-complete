69d33477407378854e90ed30b7caef986e2fbfca
INTECH\one.one.tsao <0012@coreitl.com>
2023-07-11 17:59:50 +0800

    add new folder

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText;
+    },
+  },
+});
+
+app.mount('#app');
