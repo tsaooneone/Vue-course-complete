6bd4fb1b5c01b2bc02da7ca415252779299d4006
INTECH\one.one.tsao <0012@coreitl.com>
2023-07-12 14:30:29 +0800

    Learn about the life cycle

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,20 +1,47 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
+    data() {
+        return {
+            currentUserInput: "",
+            message: "Vue is great!",
+        };
     },
-    setText() {
-      // this.message = this.currentUserInput;
-      this.message = this.$refs.userText.value;
-      // console.dir(this.$refs.userText);
+    methods: {
+        saveInput(event) {
+            this.currentUserInput = event.target.value;
+        },
+        setText() {
+            // this.message = this.currentUserInput;
+            this.message = this.$refs.userText.value;
+            // console.dir(this.$refs.userText);
+        },
     },
-  },
+    beforeCreate() {
+        console.log("beforeCreate()");
+    },
+    created() {
+      console.log('created()');
+    },
+    beforeMount() {
+      console.log("beforeMount()");
+    },
+    mounted() {
+      console.log('mounted()');
+    },
+    beforeUpdate() {
+      console.log("brforeUpdate()");
+    },
+    updated() {
+      console.log("updated()");
+    },
+    beforeUnmount() {
+      console.log("beforeUnmount()");
+    },
+    unmounted() {
+      console.log("unmounted()");
+    },
 });
 
 app.mount('#app');
+setTimeout(function() {
+  app.unmount();
+},3000);
